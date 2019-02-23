64e13c1a104ee4f6bf37dea0560566348e2d5da7
Viveksengar07 <vivek.sengar_mca18@gla.ac.in>
2019-02-23 17:11:05 +0530

    all commit

---
+++ app/src/main/java/sengar9/vivek/sengarlock/CreatePasswordActivity.java
@@ -0,0 +1,16 @@
+package sengar9.vivek.sengarlock;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class CreatePasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_password);
+
+    }
+}
---
+++ app/src/main/java/sengar9/vivek/sengarlock/InputPasswordActivity.java
@@ -0,0 +1,13 @@
+package sengar9.vivek.sengarlock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InputPasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_password);
+
+    }
+}
